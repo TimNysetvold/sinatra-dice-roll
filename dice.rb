2d79f4ee9f4c0750a086138cb617005e0075523e
TimNysetvold <tim.nysetvold@gmail.com>
2024-04-17 20:45:34 +0000

    complete

--- dice.rb
+++ dice.rb
@@ -1,11 +1,42 @@
 # /dice.rb
 
 require "sinatra"
+require "sinatra/reloader"
 
 get("/") do
-  "Hello World"
+  "<h2>Welcome to the Dice Rolling Extravaganza</h2>
+  <ul>
+    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+</ul>"
 end
 
 get("/zebra") do
   "We must add a route for each path we want to support"
 end
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
+end
+
+ get("/dice/2/10") do
+  first_die =rand(1..10)
+  second_die =rand(1..10)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h2>2d10</h2>
+   <p>#{outcome}</p>"
+ end
